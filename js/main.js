df1431c6c22cd1cd1e345b26409bc0813d17fc83
YAgmy <yasseragmy43@gmail.com>
2022-04-14 01:25:40 +0200

    Update the imgs shuffel

--- js/main.js
+++ js/main.js
@@ -2,56 +2,14 @@
 $(function () {
     $('.work li').on('click', function () {
         $(this).addClass("active rounded-pill").siblings().removeClass("active");
-    });
-    $('.designbtn').on('click', function () {
-        $(".work .row .code").fadeOut("fast"), function () {
-            $(".work .row .design").fadeIn("fast")
-        };
-        $(".work .row .app").fadeOut("fast"), function () {
-            $(".work .row .design").fadeIn("fast");
-        };
-        $(".work .row .photo").fadeOut("fast"), function () {
-            $(".work .row .design").fadeIn("fast");
-        };
-    })
-    $('.codebtn').on('click', function () {
-        $(".work .row .design").fadeOut("fast"), function () {
-            $(".work .row .code").fadeIn("fast");
-        };
-        $(".work .row .app").fadeOut("fast"), function () {
-            $(".work .row .code").fadeIn("fast");
-        };
-        $(".work .row .photo").fadeOut("fast"), function () {
-            $(".work .row .code").fadeIn("fast");
-        };
-    })
-    $('.photobtn').on('click', function () {
-        $(".work .row .design").fadeOut("fast", function () {
-            $(".work .row .photo").fadeIn("fast")
+
+            if ($(this).data("class") === 'all') {
+            $(".shuffle-imgs .col-md").show();
+            } else {
+            $(".shuffle-imgs .col-md").css("opacity", ".08");
+
+            $($(this).data("class")).parent().css("opacity", 1);
+        }
         });
-        $(".work .row .code").fadeOut("fast", function () {
-            $(".work .row .photo").fadeIn("fast");
-        });
-        $(".work .row .app").fadeOut("fast", function () {
-            $(".work .row .photo").fadeIn("fast");
-        });
-    })
-    $('.appbtn').on('click', function () {
-        $(".work .row .design").fadeOut("fast", function () {
-            $(".work .row .app").fadeIn("fast")
-        });
-        $(".work .row .code").fadeOut("fast", function () {
-            $(".work .row .app").fadeIn("fast");
-        });
-        $(".work .row .photo").fadeOut("fast", function () {
-            $(".work .row .app").fadeIn("fast");
-        });
-    })
-    $('.work ul .allbtn').on('click', function () {
-        $(".work .row .design").fadeIn("fast");
-        $(".work .row .code").fadeIn("fast");
-        $(".work .row .app").fadeIn("fast");
-        $(".work .row .photo").fadeIn("fast");
-    });
-});
+});    
     
